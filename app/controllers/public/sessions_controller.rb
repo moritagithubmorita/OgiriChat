92dafc8fff67742a1fdacb645bca436eb8874bc5
EC2 Default User <ec2-user@ip-172-31-9-44.ap-northeast-1.compute.internal>
2021-08-17 13:14:27 +0000

    public/sessionscontrollerbeforeaction順番変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  #退会顧客がログインできないようにする
-  before_action :loginable_check, only: [:create]
   # 管理者とユーザの同時ログイン状態を禁止する
   before_action :admin_signed_in_check, only: [:create]
+  #退会顧客がログインできないようにする
+  before_action :loginable_check, only: [:create]
 
   #退会済み顧客のログインを阻止する処理
   def loginable_check
