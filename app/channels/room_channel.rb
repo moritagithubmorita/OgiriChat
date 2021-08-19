6281b31d26bae0cf728ddb626ea0c0ebe2c1fd93
EC2 Default User <ec2-user@ip-172-31-9-44.ap-northeast-1.compute.internal>
2021-08-19 05:36:51 +0000

    デバックコード追加

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,9 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel_#{params[:question_room_id]}"
+
+    # デバック。本番でbinding.pryが動くか怪しかったので。
+    User.create(name: "ダミー#{Time.new.strftime("%Y-%m-%d %H:%M:%S")}", email: 'dummy@dummy', password: '000000')
   end
 
   def unsubscribed
